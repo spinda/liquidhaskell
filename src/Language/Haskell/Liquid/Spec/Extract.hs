b51afda0f296033fac5bb49ec7e8290a6db4bfd6
Michael Smith <michael@diglumi.com>
2015-06-19 01:33:40 -0700

    Extract expression parameters for type synonyms

--- src/Language/Haskell/Liquid/Spec/Extract.hs
+++ src/Language/Haskell/Liquid/Spec/Extract.hs
@@ -9,11 +9,14 @@ module Language.Haskell.Liquid.Spec.Extract (
 
 import GHC
 
+import Annotations
 import Bag
 import HsBinds
 import HsExpr
 import HscTypes
 import MonadUtils
+import NameEnv
+import Serialized
 import SrcLoc
 import TcRnTypes
 import TyCon
@@ -25,6 +28,7 @@ import Language.Fixpoint.Types
 
 import Language.Haskell.Liquid.GhcMisc
 import Language.Haskell.Liquid.RefType
+import Language.Haskell.Liquid.RType
 import Language.Haskell.Liquid.Types
 
 import Language.Haskell.Liquid.Spec.Env
@@ -76,10 +80,11 @@ idsFromValBinds (ValBindsOut binds _) = concatMap (idsFromBinds . snd) binds
 
 --------------------------------------------------------------------------------
 
-extractTySyns :: TypecheckedModule -> SpecM [RTAlias RTyVar SpecType]
-extractTySyns mod =
+extractTySyns :: TypecheckedModule -> ModGuts -> SpecM [RTAlias RTyVar SpecType]
+extractTySyns mod guts =
   mapM go tysyns
   where
+    annenv = mkAnnEnv $ mg_anns guts
     things = modInfoTyThings $ tm_checked_module_info mod
     tycons = mapMaybe (\case { ATyCon tc -> Just tc; _ -> Nothing }) things
     tysyns = mapMaybe (\tc -> (tc, ) <$> synTyConDefn_maybe tc) tycons
@@ -88,10 +93,15 @@ extractTySyns mod =
       return $
         RTA { rtName  = symbol tc
             , rtTArgs = map rTyVar tvs
-            , rtVArgs = []
+            , rtVArgs = map symbolRTyVar $ lookupVArgs annenv tc
             , rtBody  = rhs'
             -- TODO: Extract type synonym position data
             , rtPos   = dummyPos "TypeAlias"
             , rtPosE  = dummyPos "TypeAlias"
             }
+
+lookupVArgs :: AnnEnv -> TyCon -> [Symbol]
+lookupVArgs env = concatMap go . findAnns deserializeWithData env . NamedTarget . getName
+  where
+    go (ExprParams vargs) = map symbol vargs
 
