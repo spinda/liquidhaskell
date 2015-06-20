2ad2fce2629613673a026e8dc5c9aeabc58efb58
Michael Smith <michael@diglumi.com>
2015-06-19 17:41:50 -0700

    Implement expression parameters in the frontend

--- benchmarks/gsoc15/pos/test1.hs
+++ benchmarks/gsoc15/pos/test1.hs
@@ -6,14 +6,13 @@ module Test1 () where
 import LiquidHaskell
 
 [lq| type Nat = {v:Int | 0 <= v} |]
-
-type Plus x y = [lq|{v:Int | v = x + y}|]
-
-plus :: [lq| x:Int |] -> [lq| y:Int |] -> Plus x y
-plus = undefined
+[lq| type Plus x y = {v:Int | v = x + y} |]
 
 ok :: Nat -> Nat
 ok x = plus x one
+
+plus :: [lq| x:Int |] -> [lq| y:Int |] -> Plus x y
+plus = undefined
 
 one :: [lq|{v:Int | v = 1}|]
 one = undefined
