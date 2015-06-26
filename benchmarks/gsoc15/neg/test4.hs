d0082ae4b8e8542dd3e21e78326d4bf24293714f
Michael Smith <michael@diglumi.com>
2015-06-25 21:08:24 -0700

    Update expr param reification to match parser

--- benchmarks/gsoc15/neg/test4.hs
+++ benchmarks/gsoc15/neg/test4.hs
@@ -5,10 +5,11 @@ module Test3 () where
 
 import LiquidHaskell
 
-[lq| type Nat     = { v:Int | 0 <= v } |]
-[lq| type NatGE X = { v:Nat | X <  v } |]
+[lq| type Nat        = { v:Int | 0 <= v } |]
+[lq| type NatGE  a X = { v:a | X <  v } |]
+[lq| type NatGE2   X = NatGE Nat X |]
 
-bad :: [lq| x:Nat -> NatGE {x} |]
+bad :: [lq| x:Nat -> NatGE2 {x} |]
 bad x = minus x one
 
 minus :: [lq| x:Int -> y:Int -> { v:Int | v = x - y } |]
--- benchmarks/gsoc15/pos/test1.hs
+++ benchmarks/gsoc15/pos/test1.hs
@@ -6,12 +6,12 @@ module Test1 () where
 import LiquidHaskell
 
 [lq| type Nat = { v:Int | 0 <= v } |]
-[lq| type Plus x y = { v:Int | v = x + y } |]
+[lq| type Plus X Y = { v:Int | v = X + Y } |]
 
 ok :: Nat -> Nat
 ok x = plus x one
 
-plus :: [lq| x:Int |] -> [lq| y:Int |] -> Plus x y
+plus :: [lq| x:Int |] -> [lq| y:Int |] -> [lq| Plus {x} {y} |]
 plus = undefined
 
 one :: [lq| { v:Int | v = 1 } |]
